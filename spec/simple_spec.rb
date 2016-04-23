b4693bcbc0d878f2227f694ed4a433fd6c7e2741
Victor Bjelkholm <victorbjelkholm@gmail.com>
2016-04-23 09:41:53 +0200

    iFailing test on < 2.3

--- spec/simple_spec.rb
+++ spec/simple_spec.rb
@@ -1,11 +1,8 @@
 describe Integer do
-  it "Is true" do
-    expect(true).to eql(true)
+  it "2.3 Hash comparison" do
+    expect({ x: 1, y: 2 } >= { x: 1 }).to eql(true)
+    expect({ x: 1, y: 2 } >= { x: 2 }).to eql(false)
+    expect({ x: 1 } >= { x: 1, y: 2 }).to eql(false)
   end
-  #it "2.3 Hash comparison" do
-  #  expect({ x: 1, y: 2 } >= { x: 1 }).to eql(true)
-  #  expect({ x: 1, y: 2 } >= { x: 2 }).to eql(false)
-  #  expect({ x: 1 } >= { x: 1, y: 2 }).to eql(false)
-  #end
 end
 
